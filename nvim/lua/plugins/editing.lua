0d29949ac5e0e547f3d2560684014543069c17b9
ksmxxxxxx <kankichi69@gmail.com>
2026-04-07 12:21:34 +0900

    nvim: copilot.lua を追加
    - GitHub Copilot のインライン補完を導入
    - nodenv 環境下で 特定のプロジェクトの .node-version に
      影響されないよう copilot_node_command で Node 22 を直接指定
    - キーマップ: Ctrl+l 承認 / Ctrl+] 却下 / Ctrl+j 次 / Ctrl+k 前

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -1,5 +1,27 @@
 return {
   {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup({
+        copilot_node_command = vim.fn.expand('~/.nodenv/versions/22.14.0/bin/node'),
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = '<C-l>',
+            dismiss = '<C-]>',
+            next = '<C-j>',
+            prev = '<C-k>',
+          },
+        },
+        panel = { enabled = false },
+      })
+    end,
+  },
+
+  {
     'm4xshen/autoclose.nvim',
     config = function()
       require("autoclose").setup({
